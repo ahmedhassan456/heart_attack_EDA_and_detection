4be8a394d8b1c0f46d7d94b3606d937843bc4641
Ahmed Saqr <ah8081189@gmail.com>
2025-01-22 13:09:04 +0200

    deployment using flask

---
+++ deployment/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+from controller.GetPredictions import GetPredictions
+from controller.LoadModel import LoadModel
+
+app = Flask(__name__)
+
+model = LoadModel('deployment\models\heart_attack_detection_model_99.3%.pkl')
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        inputs = request.form.to_dict()
+        prediction = GetPredictions(model, inputs)
+
+        return render_template('result.html', prediction=prediction)
+
+if __name__ == '__main__':
+    app.run(debug=True)
